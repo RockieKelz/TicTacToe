59078353eabc469ed54ad5453ec58b1bd6488b5c
RockieKelz <racquy@msn.com>
2023-03-04 21:33:05 -0800

    Added position selection logic

--- TicTacToe/Game.cpp
+++ TicTacToe/Game.cpp
@@ -1,5 +1,5 @@
 #include "Game.h"
-
+#include <Windows.h>
 
 void Game::displayBoard()
 {
@@ -19,6 +19,52 @@ void Game::displayBoard()
     cout << "\t\t\t\t   |   |   " << endl;
     cout << endl;
     
+}
+
+void Game::playerInput(Player& player)
+{
+    int selectedSpot;
+    //get the player's selected space
+    cout << "\n\t\t" << player.getName() << "'s Turn: \n";
+    cout << "\t\tSelect an open position on the board " << endl;
+    cin >> selectedSpot;
+    //get the selected space's index number
+    selectedSpot -= 1;
+    //check if the space is open
+    if (emptyIndex[selectedSpot] == 1) {
+        cout << "\n-----That spot is not empty. Try Again-------" << endl;
+        playerInput(player);
+    }
+    else {
+        //mark the spot as occuppied
+        emptyIndex[selectedSpot] = 1;
+        emptyCount -= 1;
+        player.getName().compare(player1) == 0 ? board[selectedSpot] = 'X' : board[selectedSpot] = 'O';
+    }
+
+}
+
+void Game::computerInput()
+{
+    Sleep(200);
+    //randomly select a position on the board
+    int selectedSpot;
+    int n;
+    srand(time(NULL));
+    selectedSpot = rand() % 10;
+
+    //check if the space is taken
+    if (emptyIndex[selectedSpot] == 1) {
+        if (emptyCount < 0)
+            return;
+        computerInput();
+    }
+    else { //if spot is open, mark it as taken by the computer
+        cout << "\nComputer chose: " << selectedSpot + 1 << endl;
+        emptyIndex[selectedSpot] = 1;
+        emptyCount -= 1;
+        board[selectedSpot] = 'O';
+    }
 }
 
 void Game::init()
@@ -62,5 +108,28 @@ void Game::twoPlayerGame()
 
 void Game::playGame(Player &p1, Player &p2) {
     system("cls");
+    //show the game board
     displayBoard();
+
+    //set the game flags
+    gameOver = false;
+    int playerTurn = 1;
+    
+    //start the game
+    while (!gameOver)
+    {
+        //check player's turn
+        if (againstComputer) {
+            playerTurn == 1 ? playerInput(p1) : computerInput();
+        }
+        else
+        {
+            playerTurn == 1 ? playerInput(p1) : playerInput(p2);
+        }
+
+        displayBoard();
+
+        //change the current player's turn
+        playerTurn = !playerTurn;
+    }
 }
--- TicTacToe/Game.h
+++ TicTacToe/Game.h
@@ -12,11 +12,15 @@ private:
     char board[9] = { '1', '2',
     '3', '4', '5',
     '6', '7', '8', '9' };
+    int emptyIndex[9];
+    int emptyCount = 9;
     string player1 = "PLAYER 1", player2 = "PLAYER 2";
-    bool againstComputer;
+    bool againstComputer, gameOver;
     void displayBoard();
+    void playerInput(Player& player);
+    void computerInput();
+
 public:
-   
     void init();
     void onePlayerGame();
     void twoPlayerGame();
